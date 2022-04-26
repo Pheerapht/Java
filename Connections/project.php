aeca5b416de84161879f335a2ee5a1e3852b1180
Pheeraphat Jaiboon <69642654+Pheerapht@users.noreply.github.com>
2022-04-27 02:52:24 +0700

    Update project.php

--- Connections/project.php
+++ Connections/project.php
@@ -5,6 +5,6 @@
 $hostname_project = "localhost";
 $database_project = "project";
 $username_project = "root";
-$password_project = "";
+$password_project = "RYOadx55337";
 $project = mysql_pconnect($hostname_project, $username_project, $password_project) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
