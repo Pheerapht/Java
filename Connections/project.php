2804d3eb9b1dc854843e44484acfc5b48cb7e9e1
Pheeraphat Jaiboon <69642654+Pheerapht@users.noreply.github.com>
2022-04-27 11:28:30 +0700

    Update project.php

--- Connections/project.php
+++ Connections/project.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_project = "https://node31502-my-shop.app.ruk-com.cloud/";
+$hostname_project = "node31588-env-5174474.app.ruk-com.clou";
 $database_project = "project";
 $username_project = "root";
-$password_project = "RYOadx55337";
+$password_project = "ZBAaqf96816";
 $project = mysql_pconnect($hostname_project, $username_project, $password_project) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
